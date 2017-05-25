45d98f9ba298d91342e37dfe98ed568efde9cfcc
huohuo21 <1501110102@pku.edu.cn>
2017-05-25 00:04:12 +0000

    ZPKUTreeMaker.cc MET bug fix

--- PKUTreeMaker/plugins/ZPKUTreeMaker.cc
+++ PKUTreeMaker/plugins/ZPKUTreeMaker.cc
@@ -1224,28 +1224,28 @@ ZPKUTreeMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
 // Marked for debug
 //------------------central value, correction from JetuserData---------------------
-			double pxcorr = rawPx+TypeICorrMap_user_["corrEx_JEC"]+TypeICorrMap_user_["corrEx_JER"];
-                        double pycorr = rawPy+TypeICorrMap_user_["corrEy_JEC"]+TypeICorrMap_user_["corrEy_JER"];
+			double pxcorr = rawPx+TypeICorrMap_user_["corrEx_MET_JEC"]+TypeICorrMap_user_["corrEx_MET_JER"];
+                        double pycorr = rawPy+TypeICorrMap_user_["corrEy_MET_JEC"]+TypeICorrMap_user_["corrEy_MET_JER"];
                         double et     = std::hypot(pxcorr,pycorr);
-                        double sumEtcorr = rawSumEt+TypeICorrMap_user_["corrSumEt_JEC"]+TypeICorrMap_user_["corrSumEt_JER"];
+                        double sumEtcorr = rawSumEt+TypeICorrMap_user_["corrSumEt_MET_JEC"]+TypeICorrMap_user_["corrSumEt_MET_JER"];
 			//----for JEC uncertainty study
-			double pxcorr_JEC_up = rawPx+TypeICorrMap_user_["corrEx_JEC_up"]+TypeICorrMap_user_["corrEx_JER"];
-                        double pycorr_JEC_up = rawPy+TypeICorrMap_user_["corrEy_JEC_up"]+TypeICorrMap_user_["corrEy_JER"];
+			double pxcorr_JEC_up = rawPx+TypeICorrMap_user_["corrEx_MET_JEC_up"]+TypeICorrMap_user_["corrEx_MET_JER"];
+                        double pycorr_JEC_up = rawPy+TypeICorrMap_user_["corrEy_MET_JEC_up"]+TypeICorrMap_user_["corrEy_MET_JER"];
                         double et_JEC_up     = std::hypot(pxcorr_JEC_up, pycorr_JEC_up);
-                        double sumEtcorr_JEC_up = rawSumEt+TypeICorrMap_user_["corrSumEt_JEC_up"]+TypeICorrMap_user_["corrSumEt_JER"];
-			double pxcorr_JEC_down = rawPx+TypeICorrMap_user_["corrEx_JEC_down"]+TypeICorrMap_user_["corrEx_JER"];
-                        double pycorr_JEC_down = rawPy+TypeICorrMap_user_["corrEy_JEC_down"]+TypeICorrMap_user_["corrEy_JER"];
+                        double sumEtcorr_JEC_up = rawSumEt+TypeICorrMap_user_["corrSumEt_MET_JEC_up"]+TypeICorrMap_user_["corrSumEt_MET_JER"];
+			double pxcorr_JEC_down = rawPx+TypeICorrMap_user_["corrEx_MET_JEC_down"]+TypeICorrMap_user_["corrEx_MET_JER"];
+                        double pycorr_JEC_down = rawPy+TypeICorrMap_user_["corrEy_MET_JEC_down"]+TypeICorrMap_user_["corrEy_MET_JER"];
                         double et_JEC_down     = std::hypot(pxcorr_JEC_down, pycorr_JEC_down);
-                        double sumEtcorr_JEC_down = rawSumEt+TypeICorrMap_user_["corrSumEt_JEC_down"]+TypeICorrMap_user_["corrSumEt_JER"];
+                        double sumEtcorr_JEC_down = rawSumEt+TypeICorrMap_user_["corrSumEt_MET_JEC_down"]+TypeICorrMap_user_["corrSumEt_MET_JER"];
 			//----for JER uncertainty study
-			double pxcorr_JER_up = rawPx+TypeICorrMap_user_["corrEx_JEC"]+TypeICorrMap_user_["corrEx_JER_up"];
-                        double pycorr_JER_up = rawPy+TypeICorrMap_user_["corrEy_JEC"]+TypeICorrMap_user_["corrEy_JER_up"];
+			double pxcorr_JER_up = rawPx+TypeICorrMap_user_["corrEx_MET_JEC"]+TypeICorrMap_user_["corrEx_MET_JER_up"];
+                        double pycorr_JER_up = rawPy+TypeICorrMap_user_["corrEy_MET_JEC"]+TypeICorrMap_user_["corrEy_MET_JER_up"];
                         double et_JER_up     = std::hypot(pxcorr_JER_up, pycorr_JER_up);
-                        double sumEtcorr_JER_up = rawSumEt+TypeICorrMap_user_["corrSumEt__JEC"]+TypeICorrMap_user_["corrSumEt_JER_up"];
-			double pxcorr_JER_down = rawPx+TypeICorrMap_user_["corrEx_JEC"]+TypeICorrMap_user_["corrEx_JER_down"];
-                        double pycorr_JER_down = rawPy+TypeICorrMap_user_["corrEy_JEC"]+TypeICorrMap_user_["corrEy_JER_down"];
+                        double sumEtcorr_JER_up = rawSumEt+TypeICorrMap_user_["corrSumEt_MET_JEC"]+TypeICorrMap_user_["corrSumEt_MET_JER_up"];
+			double pxcorr_JER_down = rawPx+TypeICorrMap_user_["corrEx_MET_JEC"]+TypeICorrMap_user_["corrEx_MET_JER_down"];
+                        double pycorr_JER_down = rawPy+TypeICorrMap_user_["corrEy_MET_JEC"]+TypeICorrMap_user_["corrEy_MET_JER_down"];
                         double et_JER_down     = std::hypot(pxcorr_JER_down,pycorr_JER_down);
-                        double sumEtcorr_JER_down = rawSumEt+TypeICorrMap_user_["corrSumEt_JEC"]+TypeICorrMap_user_["corrSumEt_JER_JER_down"];
+                        double sumEtcorr_JER_down = rawSumEt+TypeICorrMap_user_["corrSumEt_MET_JEC"]+TypeICorrMap_user_["corrSumEt_MET_JER_down"];
 //------------------ correction from JetuserData---------------------
 // Marked for debug
 			TLorentzVector corrmet;
